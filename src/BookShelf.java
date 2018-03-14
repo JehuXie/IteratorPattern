8ccd54f3b603f38e1f1dc05fba548b5c44896714
15295626301 <15295626301>
2018-03-14 13:51:29 +0800

    迭代器设计模式

---
+++ src/BookShelf.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+    //private Book[] books;
+    private ArrayList books;
+    private int last = 0;
+
+    public BookShelf(int maxsize){
+        this.books = new ArrayList(maxsize);
+    }
+    public Book getBookAt(int index){
+        return (Book) books.get(index);
+    }
+    public void appendBook(Book book){
+        books.add(book);
+        /*this.books[last] = book;
+        last++;*/
+    }
+    public int getLength(){
+        //return last;
+        return books.size();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
---
+++ src/Iterator.java
@@ -0,0 +1,6 @@
+public interface Iterator {
+    //是否存在下一个元素
+    public abstract boolean hasNext();
+    //返回下一个元素
+    public abstract Object next();
+}
